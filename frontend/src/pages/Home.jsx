68cf492f1876f1ae55f0b5ce6f98ace523ea0413
mareaugustin <augustin.millogo.dev@gmail.com>
2025-11-20 22:44:58 +0000

    maquette gymPlus.pdf et quelques config front

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      Non implementé pour l'instant
+    </div>
+  );
+}
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="container mx-auto p-4">
+      Page non trouvée,404
+    </div>
+  );
+}
